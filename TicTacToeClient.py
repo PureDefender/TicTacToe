e6cfd5b218f7d71ff4dbc80ded8613395e390c8b
Owen Dugmore <oadugmore@outlook.com>
2020-04-30 15:36:58 -0700

    add show_board function for UI

--- TicTacToeClient.py
+++ TicTacToeClient.py
@@ -6,24 +6,35 @@ MSG_LEN = 2048
 class ClientInterface:
     def get_move(self):
         move = input('Where do you want to move?')
-        return move
+        return int(move)
 
     def show_msg(self, msg):
         print(msg)
+
+    def show_board(self, board):
+        for i in range(3):
+            for j in range(3):
+                print(board[i * 3 + j], end=' ')
+            print()
 
 class TicTacToeClient:
     def __init__(self, server_address):
         #super().__init__()
         self.conn = socket.create_connection(server_address)
         self.io = ClientInterface()
+        self.board = ['+'] * 9
     
     def play(self):
         response : str = self.recv_msg()
+        print('Initial response: ' + response)
         mark = response[8]
         opponent_mark = 'O' if mark == 'X' else 'X'
         self.io.show_msg('You are player ' + mark)
+
+        # O goes first
         if mark == 'O':
             self.play_move()
+            
         while 1:
             response = self.recv_msg()
             if response == '':
@@ -31,11 +42,12 @@ class TicTacToeClient:
                 break
             if response.startswith('VALID_MOVE'):
                 self.io.show_msg('Valid move, please wait for opponent.')
-                # update board using self.current_square
+                self.board[self.current_square] = mark
+                self.io.show_board(self.board)
             elif response.startswith('OPPONENT_MOVED'):
                 loc = int(response[15:])
-                # update board using loc
-                self.io.show_msg('MOVE: ' + loc)
+                self.board[loc] = opponent_mark
+                self.io.show_board(self.board)
                 self.io.show_msg('Opponent moved, your turn.')
             elif response.startswith('MSG'):
                 self.io.show_msg(response[4:])
@@ -54,11 +66,12 @@ class TicTacToeClient:
 
             self.play_move()
         
+        self.send_msg('QUIT')
         self.io.show_msg('Game has ended.')
 
     def play_move(self):
         self.current_square = self.io.get_move()
-        msg = 'MOVE' + self.current_square
+        msg = 'MOVE ' + self.current_square
         self.send_msg(msg)
 
     def send_msg(self, msg: str):
